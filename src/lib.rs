f142a046a04f1e926aed0c2139267e6190c7dad2
Sonia Pignorel <396009+soniapi@users.noreply.github.com>
2025-11-29 11:10:19 -0800

    feat(lib): add computation of complex multiplication for odd and even numbers

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,40 @@ pub fn dtft_item_k(input_seq: &[Complex<f64>], k: i32) -> Complex<f64> {
     output
 }
 
+pub fn fft_item_n(input_seq: &[Complex<f64>], k: i32, n: i32) -> Complex<f64>{
+    let even_index = 2 * n as usize;
+    let even_item = &input_seq[even_index];
+    println!("Even item {:?} for {:?}", even_item, n);
+    let odd_index = (2 * n + 1) as usize;
+    let odd_item = &input_seq[odd_index];
+    println!("Odd item {:?} for {:?}", odd_item, n);
+
+    let number_even: Complex<f64> = Complex::<f64>::i()
+            .scale(2.0)
+            .scale(-1.0)
+            .scale(PI)
+            .scale(k as f64 / input_seq.len() as f64)
+            .scale((2 * n) as f64);
+
+    let exponential_even: Complex<f64> = number_even.exp();
+    let complex_mult_even: Complex<f64> = *even_item * exponential_even as Complex<f64>;
+
+    let number_odd: Complex<f64> = Complex::<f64>::i()
+            .scale(2.0)
+            .scale(-1.0)
+            .scale(PI)
+            .scale(k as f64 / input_seq.len() as f64)
+            .scale((2 * n + 1) as f64);
+
+    let exponential_odd: Complex<f64> = number_odd.exp();
+    let complex_mult_odd: Complex<f64> = *odd_item * exponential_odd as Complex<f64>;
+
+    let output = complex_mult_even + complex_mult_odd;
+    println!("output {:?} for k {:?} and n {:?}", output, k, n);
+    output
+
+}
+
 pub fn dtft(input_seq: &[Complex<f64>]) -> Vec<Complex<f64>> {
     let mut output_seq = Vec::new();
 
